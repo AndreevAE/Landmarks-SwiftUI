099001f31a1c17703503cae35fa441504d815a22
Alexander Andreev <andr33v-ae@yandex.ru>
2020-06-22 06:35:36 +0700

    Section 4. Use an Observable Object for Storage

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Александр Андреев on 22.06.2020.
+//  Copyright © 2020 Александр Андреев. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
