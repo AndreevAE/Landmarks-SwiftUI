d4e586df8e5f7782a389d4dbb5f3c37f4ddcfcce
Alexander Andreev <andr33v-ae@yandex.ru>
2020-06-22 08:04:38 +0700

    Animating Views and Transitions starting point

--- Landmarks/Models/Data.swift
+++ Landmarks/Models/Data.swift
@@ -1,30 +1,32 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+ See LICENSE folder for this sample’s licensing information.
 
-Abstract:
-Helpers for loading images and data.
-*/
+ Abstract:
+ Helpers for loading images and data.
+ */
 
+import Foundation
+import CoreLocation
 import UIKit
 import SwiftUI
-import CoreLocation
 
 let landmarkData: [Landmark] = load("landmarkData.json")
+let hikeData: [Hike] = load("hikeData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
         else {
             fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
@@ -38,12 +40,12 @@ final class ImageStore {
     fileprivate var images: _ImageDictionary = [:]
 
     fileprivate static var scale = 2
-    
+
     static var shared = ImageStore()
-    
+
     func image(name: String) -> Image {
         let index = _guaranteeImage(name: name)
-        
+
         return Image(images.values[index], scale: CGFloat(ImageStore.scale), label: Text(name))
     }
 
@@ -52,15 +54,15 @@ final class ImageStore {
             let url = Bundle.main.url(forResource: name, withExtension: "jpg"),
             let imageSource = CGImageSourceCreateWithURL(url as NSURL, nil),
             let image = CGImageSourceCreateImageAtIndex(imageSource, 0, nil)
-        else {
-            fatalError("Couldn't load image \(name).jpg from main bundle.")
+            else {
+                fatalError("Couldn't load image \(name).jpg from main bundle.")
         }
         return image
     }
-    
+
     fileprivate func _guaranteeImage(name: String) -> _ImageDictionary.Index {
         if let index = images.index(forKey: name) { return index }
-        
+
         images[name] = ImageStore.loadImage(name: name)
         return images.index(forKey: name)!
     }
