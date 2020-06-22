40c0f7868288242d428d014be8cd5e8fe8819765
Alexander Andreev <andr33v-ae@yandex.ru>
2020-06-22 07:03:10 +0700

    Section 1. Create a Badge View

---
+++ Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Александр Андреев on 22.06.2020.
+//  Copyright © 2020 Александр Андреев. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
